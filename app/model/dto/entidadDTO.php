851827592e4859f484431c6d98bbd958abb5f334
jmurilloariza <jmurilloariza@gmail.com>
2016-12-01 08:51:54 -0500

    Hice daos

---
+++ app/model/dto/entidadDTO.php
@@ -0,0 +1,18 @@
+<?php
+
+class entidadDTO{
+    
+    private $cod_entidad;
+    private $nombre;
+    private $direccion;
+    private $ciudad;
+    private $cod_sucursal;
+    
+    
+    public function __construct($cod_entidad, $nombre, $direccion, $ciudad, $cod_sucursal){
+        
+        $this->cod_entidad = $cod
+    }
+}
+
+?>
