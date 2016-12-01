0a88154c3bcffd98ef250ba988b56e1c08106deb
jmurilloariza <jmurilloariza@gmail.com>
2016-12-01 09:44:31 -0500

    dtos

--- app/model/dto/entidadDTO.php
+++ app/model/dto/entidadDTO.php
@@ -1,6 +1,6 @@
 <?php
 
-class entidadDTO{
+class EntidadDTO{
     
     private $cod_entidad;
     private $nombre;
@@ -10,9 +10,53 @@ class entidadDTO{
     
     
     public function __construct($cod_entidad, $nombre, $direccion, $ciudad, $cod_sucursal){
-        
-        $this->cod_entidad = $cod
+        $this->cod_entidad = $cod_entidad;
+        $this->nombre = $nombre;
+        $this->direccion = $direccion;
+        this->ciudad = $ciudad;
+        this->cod_sucursal = $cod_sucursal;
     }
+    
+    public function getCod_entidad(){
+        return $this->cod_entidad;
+    }
+    
+    public function setCod_entidad($cod_entidad){
+        $this->cod_entidad = $cod_entidad;
+    }
+    
+    public function getNombre(){
+        return $this->nombre;
+    }
+    
+    public function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+    
+    public function getDireccion(){
+        return $this->direccion;
+    }
+    
+    public function setDireccion($direccion){
+        $this->direccion $direccion;
+    }
+    
+    public function getCiudad(){
+        return $this->ciudad;
+    }
+    
+    public function setCiudad($ciudad){
+        $this->ciudad = $ciudad;
+    }
+    
+    public function getCod_sucursal(){
+        return $this->cod_sucursal;
+    }
+    
+    public function setCod_sucursal($cod_sucursal){
+        $this->cod_sucursal = $cod_sucursal;
+    }
+    
 }
 
 ?>
--- app/model/model.php
+++ app/model/model.php
@@ -4,10 +4,10 @@ class Model {
     private $connection;
 
     public function connect(){
-        $server="localhost";
-        $user="root";
-        $pass="";
-        $bd="prev2";
+        $server="http://sandbox2.ufps.edu.co/phpmyadmin/index.php";
+        $user="1151224";
+        $pass="1151224";
+        $bd="1151224";
         $this->connection = mysqli_connect($server,$user,$pass,$bd) or  die(("Error " . mysqli_error($this->connection)));
     }
 
